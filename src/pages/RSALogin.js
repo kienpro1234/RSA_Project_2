609dda60513d403e3bc4d880a3661456acb71a60
kienpro <nguyenduckien1109@gmail.com>
2024-06-12 18:59:57 +0700

    push code

---
+++ src/pages/RSALogin.js
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import {
+  UserOutlined,
+  LockOutlined,
+  EyeTwoTone,
+  EyeInvisibleOutlined,
+} from "@ant-design/icons";
+import { Button, Input } from "antd";
+import { withFormik } from "formik";
+import * as Yup from "yup";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+// import { auth } from "../FireBase/FireBaseConfig/fireBaseConfig";
+// import { signInWithEmailAndPassword } from "firebase/auth";
+// import { RSAglobalNavigate } from "../util/RSAGlobalNavigate";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../FireBase/FireBaseConfig/fireBaseConfig";
+import Swal from 'sweetalert2';
+
+// export let loggined = false;
+export default function LoginCyberBugs(props) {
+  // const { values, touched, errors, handleChange, handleBlur, handleSubmit } =
+  //   props;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  async function handleSignIn(e) {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+    .then((user) => {
+      console.log("user",user)
+      navigate("/home");
+      localStorage.setItem("loggined", "true");
+      localStorage.setItem("email", JSON.stringify(email))
+      dispatch({
+        type: "LOGGINED_SUCCESS",
+        loggined: true,
+      })
+    })
+    .catch((err) => {
+      Swal.fire({
+        title: "Login failed! Please check your email and password again!",
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
+      localStorage.setItem("loggined", "false");
+      dispatch({
+        type: "LOGGINED_FAILED",
+        loggined: false,
+      })
+    })
+  }
+
+
+  return (
+    <form onSubmit={handleSignIn} className="container">
+      <div
+        className="d-flex flex-column justify-content-center align-items-center"
+        style={{ height: window.innerHeight }}
+      >
+        <h3
+          style={{ fontWeight: 300, fontSize: 35 }}
+          className="text-center mb-4"
+        >
+          Login
+        </h3>
+        <div>
+          <Input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            name="email"
+            style={{ minWidth: "400px" }}
+            size="large"
+            placeholder="Email"
+            prefix={<UserOutlined />}
+          />
+        </div>
+        {/* <div className="text-danger">{errors.email}</div> */}
+        <div>
+          <Input.Password
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            name="password"
+            style={{ minWidth: "400px" }}
+            className="mt-3"
+            iconRender={(visible) =>
+              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+            }
+            size="large"
+            placeholder="Password"
+            prefix={<LockOutlined />}
+          />
+        </div>
+        {/* <div className="text-danger">{errors.password}</div> */}
+        <div className="row"></div>
+        <div className="d-flex flex-row">
+          <Button
+            htmlType="submit"
+            className="mt-3"
+            style={{ backgroundColor: "rgb(102,117,223)", minWidth: 400 }}
+            type="primary"
+            size="large"
+          >
+            Login
+          </Button>
+        </div>
+        <div
+          className="mt-3 d-flex flex-row text-start"
+          style={{ minWidth: "400px", fontSize: "20px" }}
+        >
+          <p className="me-4">Don't have an account ?</p>
+          <NavLink to={"/register"}>Sign Up</NavLink>
+        </div>
+        <div className="mt-3">
+          <Button
+            style={{ backgroundColor: "rgb(59,89,142)" }}
+            className="me-3"
+            type="primary"
+            size="large"
+            shape="circle"
+          >
+            <i className="fab fa-facebook-f"></i>
+          </Button>
+          <Button type="primary" size="large" shape="circle">
+            <i className="fab fa-twitter"></i>
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+
---
+++ src/pages/RSARegister.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import {
+  UserOutlined,
+  LockOutlined,
+  EyeTwoTone,
+  EyeInvisibleOutlined,
+} from "@ant-design/icons";
+import { Button, Input } from "antd";
+import { withFormik } from "formik";
+import * as Yup from "yup";
+import { connect } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../FireBase/FireBaseConfig/fireBaseConfig";
+import Swal from 'sweetalert2'
+
+
+export default function RegisterCyberBugs(props) {
+  // const { values, touched, errors, handleChange, handleBlur, handleSubmit } =
+  //   props;
+  // const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function handleSubmit(e) {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((user) => {
+      console.log(user);
+      Swal.fire({
+        title: 'Register Success!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+      Swal.fire({
+        title: 'Register failed!',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="container">
+      <div
+        className="d-flex flex-column justify-content-center align-items-center"
+        style={{ height: window.innerHeight }}
+      >
+        <h3
+          style={{ fontWeight: 300, fontSize: 35 }}
+          className="text-center mb-4"
+        >
+          Sign Up
+        </h3>
+        <div>
+          <Input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            name="email"
+            style={{ minWidth: "400px" }}
+            size="large"
+            placeholder="Email"
+            prefix={<UserOutlined />}
+          />
+        </div>
+        {/* <div className="text-danger ">{errors.email}</div> */}
+        <div>
+          <Input.Password
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            name="password"
+            style={{ minWidth: "400px" }}
+            className="mt-3"
+            iconRender={(visible) =>
+              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+            }
+            size="large"
+            placeholder="Password"
+            prefix={<LockOutlined />}
+          />
+        </div>
+        {/* <div className="text-danger">{errors.password}</div> */}
+        <div className="row"></div>
+        <div className="d-flex flex-row">
+          <Button
+            htmlType="submit"
+            className="mt-3"
+            style={{ backgroundColor: "rgb(102,117,223)", minWidth: 400 }}
+            type="primary"
+            size="large"
+          >
+            Sign Up
+          </Button>
+        </div>
+        <div
+          className="mt-3 d-flex flex-row text-start"
+          style={{ minWidth: "400px", fontSize: "20px" }}
+        >
+          <p className="me-4">Already have an account?</p>
+          <NavLink to={"/login"}>Sign in</NavLink>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+
